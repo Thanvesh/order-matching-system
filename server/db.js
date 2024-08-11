e39638de5d985c8bd8dab729854174b75399c301
Thanvesh <nanicherry2312@gmail.com>
2024-08-12 04:17:24 +0530

    new3

--- server/db.js
+++ server/db.js
@@ -4,9 +4,9 @@ require('dotenv').config();
 const connection = mysql.createConnection({
   host:'0.tcp.in.ngrok.io',  // Your MySQL host
   port:11553,
-  user: 'root',       // Your MySQL username
-  password: 'Thani@1997',       // Your MySQL password
-  database: 'order_matching'  // Your MySQL database name
+  user:'root',       // Your MySQL username
+  password:'Thani@1997',       // Your MySQL password
+  database:'order_matching'  // Your MySQL database name
 });
 
 connection.connect((err) => {
