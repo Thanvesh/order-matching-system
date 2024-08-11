91a6c5147d358dc4495ea6d97eb0be192608799e
Thanvesh <nanicherry2312@gmail.com>
2024-08-12 01:18:32 +0530

    new

--- server/db.js
+++ server/db.js
@@ -1,8 +1,8 @@
 const mysql = require('mysql2');
 
 const connection = mysql.createConnection({
-  host: 'tcp://0.tcp.in.ngrok.io',  // Your MySQL host
-  port:11036 ,
+  host:'0.tcp.in.ngrok.io',  // Your MySQL host
+  port:11553,
   user: 'root',       // Your MySQL username
   password: 'Thani@1997',       // Your MySQL password
   database: 'order_matching'  // Your MySQL database name
