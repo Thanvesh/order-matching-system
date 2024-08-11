78278b81a693965624ddd4236fe0940420d01e36
Thanvesh <nanicherry2312@gmail.com>
2024-08-11 18:11:54 +0530

    first

---
+++ server/db.js
@@ -0,0 +1,18 @@
+const mysql = require('mysql2');
+
+const connection = mysql.createConnection({
+  host: 'localhost',  // Your MySQL host
+  user: 'root',       // Your MySQL username
+  password: 'Thani@1997',       // Your MySQL password
+  database: 'order_matching'  // Your MySQL database name
+});
+
+connection.connect((err) => {
+  if (err) {
+    console.error('Error connecting to MySQL:', err.stack);
+    return;
+  }
+  console.log('Connected to MySQL as id ' + connection.threadId);
+});
+
+module.exports = connection;
